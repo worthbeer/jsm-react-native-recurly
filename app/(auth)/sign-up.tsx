e06ab56b4cce278121399095984497292d17015a
worthbeer <67826062+worthbeer@users.noreply.github.com>
2026-03-31 14:17:28 -0400

    first commit

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+            <Link href="/(auth)/sign-up">Create Account</Link>
+            "
+        </View>
+    )
+}
+export default SignUp
