216233d7970dbce8338cf9fa5996281b7e6e4dcc
worthbeer <67826062+worthbeer@users.noreply.github.com>
2026-04-03 08:45:01 -0400

    json layout global

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,32 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import '@/global.css'
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf')
+  })
+  useEffect(() => {
+    // Hide splash only when both fonts and auth are loaded
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  // Don't render app until both are ready
+  if (!fontsLoaded) return null;
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
+
+
+
+
